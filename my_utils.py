711cf035f3020cbd0a5c2ad00ca2b1cc149c6497
Duncan Mullier <D.Mullier@leedsbeckett.ac.uk>
2023-11-09 10:28:26 +0000

    Added calc averages and an example in the library.

--- my_utils.py
+++ my_utils.py
@@ -1,3 +1,4 @@
+import math
 def average(values):
     """ Calculates the average of the given list. """
     total = 0;
@@ -5,6 +6,25 @@ def average(values):
         total += float(n)
     return total/len(values)	# return calculated average
 
+def area_rectangle(width, length):
+    area = width * length
+    return area
 
-# initialisation statement
-print("Welcome, utils module has been imported and initialised!")
+def area_square(length):
+    area = area_rectangle(length, length)
+    return area
+
+def area_circle(radius):
+    area = math.pi*(radius ** 2)
+    return area
+
+def area_triangle(base, height):
+    area = area_rectangle(base, height)/2
+    return area
+
+if __name__ == '__main__':
+    print("Area module")
+    print("area of a circle with radius of 10 ", area_circle(10))
+    print("area of a rectangle of 10,10 ", area_rectangle(10, 10))
+    print("area of a square 10 ", area_square(10))
+    print("area of a triangle 10,10 ", area_triangle(10, 10))
--- utils_test.py
+++ utils_test.py
@@ -1,3 +1,8 @@
 import my_utils
-print("Average is", my_utils.average([10, 23, 30]))
-print("Another average is", my_utils.average([10.2, 8.8, 2.6]))
+
+if __name__ == '__main__':
+    ave = my_utils.average([10, 23, 30])
+    print("Average is", ave)
+    print("Another average is", my_utils.average([10.2, 8.8, 2.6]))
+    area = my_utils.area_rectangle(10,20)
+    print("area = ",area)
